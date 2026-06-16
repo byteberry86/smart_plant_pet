411fd7ce9aaab7be2ed4d1cac9cab881d4965fee
Marianna <marianna.karagiannis8@gmail.com>
2026-06-16 15:21:06 -0400

    Update plant_bestie.ino

--- firmware/plant_bestie.ino
+++ firmware/plant_bestie.ino
@@ -1,15 +1,33 @@
 #define PIR_PIN 7 
 #define LED_OUT 6
+//define soil moisture pin 
 
 void setup() {
   pinMode(PIR_PIN, INPUT);
   pinMode(LED_OUT, OUTPUT);
+  //define soil moisture pin input 
   Serial.begin(9600);
   delay(10000);
 }
 
 void loop() {
   int motion = digitalRead(PIR_PIN);
-    digitalWrite(LED_OUT, motion);
+  //read soil moisture level
+
+  //if soil moisture ok
+    //LED off
+    digitalWrite(LED_OUT, motion); //turn on TFT screeen if motion is high (load happy png x3 shuffle) 
+
+  //if soil moiture medium 
+    //LED on
+    //turn on TFT screen if motion is high (load png medium x3 shuffle)
+  
+
+  //if soil moisture bad
+    //blink LED rapidly
+    //turn on TFT screen if motion is high (load png dry x3 shuffle)
+
+  
+  
 delay(100);
 }
