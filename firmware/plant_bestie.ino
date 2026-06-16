91692b9d19b7c7c4aef68f737cb3000310555819
Marianna <marianna.karagiannis8@gmail.com>
2026-06-15 22:46:42 -0400

    Update plant_bestie.ino

--- firmware/plant_bestie.ino
+++ firmware/plant_bestie.ino
@@ -1,1 +1,15 @@
+#define PIR_PIN 7 
+#define LED_OUT 6
 
+void setup() {
+  pinMode(PIR_PIN, INPUT);
+  pinMode(LED_OUT, OUTPUT);
+  Serial.begin(9600);
+  delay(10000);
+}
+
+void loop() {
+  int motion = digitalRead(PIR_PIN);
+    digitalWrite(LED_OUT, motion);
+delay(100);
+}
